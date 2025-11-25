f208e9aa827bc90ea3a183f205ad050278460755
Jael <josinkai@umich.edu>
2025-11-24 21:47:53 -0500

    algorithm drop down added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken")
 const bcrypt = require('bcrypt')
+const cookieParser = require("cookie-parser")
 const express = require('express')
 const db = require("better-sqlite3")("ourApp.db");
 db.pragma("journal_mode = WAL")
@@ -26,9 +27,22 @@ const app = express()
 app.set("view engine","ejs")
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"))
+app.use(cookieParser())
+
 
 app.use(function (req,res,next) {
     res.locals.errors = []
+    //try to decode incoming cookie
+    try{
+        const decoded = jwt.verify(req.cookies.ourSimpleApp,process.env.JWTSECRET)
+        req.user = decoded
+    } catch (err){
+        req.user = false
+    }
+
+    res.locals.user = req.user
+    console.log(req.user)
+
     next()
 })
 
